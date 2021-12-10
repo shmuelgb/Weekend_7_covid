9fc35d5e77044a87cf9ec81d03a213618e36c882
shmulgb <93119163+shmuelgb@users.noreply.github.com>
2021-12-10 15:01:10 +0200

    canvas chart

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 //variables
-const state = { currentChart: null };
+const state = { dataType: "confirmed" };
 const continentsList = ["asia", "europe", "africa", "america"];
 const countriesMap = {};
 const covidPerContinentMap = {};
 const covidPerCountryMap = {};
 const cors = "https://intense-mesa-62220.herokuapp.com/";
 
-//DOM variables
+//DOM Elements
 const continentsEL = document.querySelectorAll("[data-continent]");
+const dataTypeEL = document.querySelectorAll("[data-type]");
 const countriesContainerEl = document.querySelectorAll(".countries-container");
+const chartContainerEl = document.querySelector(".chart-container");
 
 //classes
 function Country(name, code) {
@@ -32,19 +34,28 @@ class CovidData {
 continentsEL.forEach((continent) => {
   continent.addEventListener("click", (e) => {
     getData(e.target.dataset.continent);
-    // create continent chart
-    //update current chart
+    createChart(e.target.dataset.continent);
+    // update current chart
   });
 });
 
+dataTypeEL.forEach((dataType) => {
+  dataType.addEventListener("click", (e) => {
+    state.dataType = e.target.dataset.type;
+    createChart(e.target.dataset.continent);
+  });
+});
+
+//check if data exists. if not, get it from api
 function getData(continent) {
-  if (countriesMap.continent) {
+  if (countriesMap[continent]) {
     return;
   } else {
     getApi(continent);
   }
 }
 
+//get data from api, store in variables
 async function getApi(continent) {
   const countries = [];
   try {
@@ -54,14 +65,14 @@ async function getApi(continent) {
     countriesApi.data.forEach((country) => {
       countries.push(new Country(country.name.common, country.cca2));
     });
-    countriesMap.continent = countries;
+    countriesMap[continent] = countries;
     getCovidData(countries, continent);
-    console.log(countries);
   } catch (error) {
     console.log(error);
   }
 }
 
+//get covid data by countries and store in variables
 async function getCovidData(countries, continent) {
   try {
     fetchAll(countries, continent);
@@ -70,6 +81,7 @@ async function getCovidData(countries, continent) {
   }
 }
 
+//fetch multiple simultaneously
 async function fetchAll(countries, continent) {
   const countriesCodes = countries.map((country) => {
     return axios.get(`${cors}https://corona-api.com/countries/${country.code}`);
@@ -85,5 +97,52 @@ async function fetchAll(countries, continent) {
       latestData.critical
     );
   });
-  console.log(covidPerCountryMap);
+  amendData(continent);
+  createChart(continent);
+}
+
+//countries and covid API's are somewhat inconsistent. fix it:
+function amendData(continent) {
+  countriesMap[continent].forEach((country) => {
+    if (covidPerCountryMap[country.name] === undefined) {
+      covidPerCountryMap[country.name] = new CovidData(continent, 0, 0, 0, 0);
+    }
+    if (country.code === "XK") country.code = null;
+  });
+}
+
+function createChart(continent) {
+  if (countriesMap[continent]) {
+    const chartEl = document.createElement("canvas");
+    chartContainerEl.appendChild(chartEl);
+    chartEl.setAttribute("class", "");
+    const chart = new Chart(chartEl, {
+      type: "line",
+      data: {
+        labels: countriesMap[continent].map((country) => country.name),
+        datasets: [
+          {
+            label: state.dataType,
+            data: getCovidDataPerContinent(continent, state.dataType),
+          },
+        ],
+      },
+      options: {
+        title: {
+          display: true,
+          text: "bla bla",
+        },
+        legend: {
+          position: "right",
+        },
+      },
+    });
+  }
+}
+
+function getCovidDataPerContinent(continent, type) {
+  const covidData = countriesMap[continent].map((country) => {
+    return covidPerCountryMap[country.name][type];
+  });
+  return covidData;
 }
